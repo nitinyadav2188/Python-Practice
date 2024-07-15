67a3e839b384e4b72ca6336bcb749477e0a6dad4
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-15 21:40:19 +0530

    Add files via upload

---
+++ Chapter-1 PS/problem-3.py
@@ -0,0 +1,8 @@
+#Question-3: install an externall module and use it to perform an operation of your interest.
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello Saurya Yadav,how are you baby?")
+engine.runAndWait()
+
+#pip install pyttsx3
