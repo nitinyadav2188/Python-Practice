3cf58730cdada6431f7c25e52f98eb56d1947a6c
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-15 21:38:36 +0530

    Create module.py

---
+++ Chapter-1/module.py
@@ -0,0 +1,9 @@
+import pyjokes
+
+# this is single-line comment
+print("Printing Jokes.......")
+joke =pyjokes.get_joke()
+print(joke)
+
+""" this is a multi-line comments
+     which is amazing."""
