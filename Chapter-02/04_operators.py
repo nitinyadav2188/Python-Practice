82313c9be1bb3a66919101445121449d3ee18ad7
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-16 11:22:47 +0530

    Add files via upload

---
+++ Chapter-02/04_operators.py
@@ -0,0 +1,34 @@
+#Arithmetic Operators:
+a=7
+b=5
+c=a+b
+print(c)
+
+#Assignment Operators:
+a=4-2 #assign 4-2 in a
+print(a)
+b=6
+b+=3 #increment in the value of b by 3 and then assign it to b
+print(b)
+
+#Comparison Operators:
+d=5>4
+print(d)
+
+#Logical Operators:
+e=True or False   #T+F=T
+
+print("Truth table of OR-------->")
+print("True or False is:",True or False)
+print("True or True is:",True or True)
+print("False or True is:",False or True)
+print("False or False is:",False or False)
+
+print("Truth table of AND------>")
+print("True and False is:",True and False)
+print("True and True is:",True and True)
+print("False and True is:",False and True)
+print("False and False is:",False and False)
+print(e)
+
+print(not(True)) #true ko false aur false ko true bna de use 'not' operator kahte hai
