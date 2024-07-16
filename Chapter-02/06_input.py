b1cc0ba65c6861f556621692826feb6dba667e8c
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-16 11:57:03 +0530

    Add files via upload

---
+++ Chapter-02/06_input.py
@@ -0,0 +1,6 @@
+a=int(input("Enter number1:")) #input string k form me input leta hai
+b=int(input("Enter number2:"))
+
+print("Number a is:",a)
+print("Number b is:",b)
+print("Sum is:",a+b) # "a"+"b"=ab--->concat
