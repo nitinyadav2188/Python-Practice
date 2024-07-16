04856c757b5a940585a25d4edce8899bce806d3b
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-16 11:15:31 +0530

    Add files via upload

---
+++ Chapter-02/02_datatypes.py
@@ -0,0 +1,5 @@
+a=71 #identifies a as class <int>
+b=88.87 #identifies b as class <float>
+name="Nitin"  #identifies name as class <String>
+d=False #identifies d as class <Boolean>
+e=None  #identifies e as class <None> 
