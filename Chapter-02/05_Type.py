6e48513da0e8a8250db0b19fc92043d220574d7a
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-16 11:56:42 +0530

    Add files via upload

---
+++ Chapter-02/05_Type.py
@@ -0,0 +1,20 @@
+a=31
+t=type(a) #type fun()-->used to define class of datatype
+print(t)
+
+b=31.234
+t=type(b) 
+print(t)
+
+c="nitin"
+t=type(c) 
+print(t)
+
+a="31"
+t=type(a) 
+print(t)
+
+a="31.56"
+b=float(a) #a but the type should be float
+t=type(b) 
+print(t)
