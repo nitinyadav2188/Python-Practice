2ca28c6987ed2db56b4fc66210a12a632d5a84d8
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-16 11:15:50 +0530

    Add files via upload

---
+++ Chapter-02/03_Rules_for_variables_naming.py
@@ -0,0 +1,6 @@
+aaa=435
+nitin=345   #variable names can start with alphabets also
+_mahek=78   #variable names can start with underscore also
+
+#9niy=djh ------------>variable name cannot start with numerals
+# n@iy=djh ----------->variable name cannot contain any special characters
