d9ac259a1478f525026ca46bd074fa560fe3a06f
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-17 14:04:03 +0530

    Add files via upload

---
+++ Chapter-02 PS/problem-06.py
@@ -0,0 +1,7 @@
+# Question-06:Write a python program to calculate the square of a number entered by the user
+
+a=int(input("Enter your number:"))
+print("Number is:",a)
+sqr=a**2
+# sqr=a^2 --->not valid in python
+print("Square of the number is:",sqr)
