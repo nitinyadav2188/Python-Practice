30cd4125d35abfb24028be90296f8038e209aaf1
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-17 14:03:25 +0530

    Add files via upload

---
+++ Chapter-02 PS/problem-04.py
@@ -0,0 +1,8 @@
+""""Question-04: Use comparison operator to find out whether ‘a’ given variable a is greater than 
+‘b’ or not. Take a = 34 and b = 80"""
+
+a=int(input("Enter number1:"))
+b=int(input("Enter number2:"))
+print("Number1:",a)
+print("Number2:",b)
+print("a is greater than b is:",a>b)
