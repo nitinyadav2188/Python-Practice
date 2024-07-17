287ab27f9ed267a977be99335ab0ad6f51fe5630
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-17 14:03:44 +0530

    Add files via upload

---
+++ Chapter-02 PS/problem-05.py
@@ -0,0 +1,9 @@
+# Question-05:Write a python program to find an average of two numbers entered by the user
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+print("Number1:",a)
+print("Number2:",b)
+avg=(a+b)/2
+print("Sum is:",avg)
