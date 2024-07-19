7d1ef3bc8f0f9b44c563cbca01641f0c6ff6a3f6
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-19 11:34:29 +0530

    Add files via upload

---
+++ Chapter-03/04_escape.py
@@ -0,0 +1,7 @@
+a="Nitin is a good boy \n but not a bad \"boy\" " #\n----> for new line
+
+print(a)
+
+#Escape Sequence Characters:
+#Sequence of characters after backslash "\" → Escape Sequence characters
+#Escape Sequence characters comprise of more than one character but represent one character when used within the strings.
