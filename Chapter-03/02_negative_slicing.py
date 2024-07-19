409ba4d728dd528660f74ee921f52dd42924ea98
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-19 11:33:15 +0530

    Add files via upload

---
+++ Chapter-03/02_negative_slicing.py
@@ -0,0 +1,8 @@
+name="Nitin"
+print(name[0:3])
+
+print(name[-4:-1]) #convert negative indices in positive indices
+print(name[1:4])
+
+print(name[:4]) #zero, is same as [0:4]
+print(name[1:]) #length, is same as [1:len]
