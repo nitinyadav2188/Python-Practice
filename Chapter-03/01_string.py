cfdc2c43cb736e5e21a8a88404dd5161ee2e3771
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-19 11:32:17 +0530

    Create 01_string.py

---
+++ Chapter-03/01_string.py
@@ -0,0 +1,13 @@
+#String is a data type in python.
+#String is a sequence of characters enclosed in quotes.
+#We can primarily write a string in these three ways.
+
+name="Nitin"
+nameshort=len(name)
+
+#string slicing:
+nameshort = name[0:3] #start from index 0 all the way till 3 (excluding 3)
+print(nameshort)
+
+character1=name[1]
+print(character1)
