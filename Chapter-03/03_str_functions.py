c6579e602f28eba0c37dd161646af6a52a698aeb
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-19 11:33:55 +0530

    Add files via upload

---
+++ Chapter-03/03_str_functions.py
@@ -0,0 +1,5 @@
+name="nitin"
+print(len(name)) #This function returns the length of the strings
+print(name.endswith("in"))#This function_ tells whether the variable string ends wit "in"
+print(name.startswith("ni"))#counts the total number of occurrences of any character.
+print(name.capitalize())#the first character of a given string.
