4232c0fcc9131eba54bb289cbb13931f030e8b3c
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-07-24 20:08:10 +0530

    Delete problem-05.py

--- problem-05.py
+++
@@ -1,5 +0,0 @@
-#Question-05:Write a program to format the following letter using escape sequence characters.
-            #letter = "Dear Nitin, this python course is nice. Thanks!"
-
-letter = "Dear Nitin,\n\t This python course is nice.\n Thanks!"
-print(letter)
