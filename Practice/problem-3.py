98bca144de985dca174d5da554c89a4ba192314c
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-08-16 21:29:17 +0530

    Add files via upload

---
+++ Practice/problem-3.py
@@ -0,0 +1,8 @@
+#to input length and breadth of a rectangle and calculate its area
+length=float(input("Enter length of the rectangle:")) 
+breadth=float(input("Enter breadth of the rectangle:")) 
+area=length*breadth
+print("Rectangle specifications")
+print("Length=",length,end='')
+print("breadth=",breadth)
+print("Area:",area)
