a93b888d4c9848e61e812f9fd5b3a3450b5dcb4a
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-08-16 21:32:34 +0530

    Add files via upload

---
+++ Practice/problem-6.py
@@ -0,0 +1,6 @@
+#write a program to input a value in kilometers and convert it into miles(1km=0.621371)
+
+km=int(input("Enter kilometers:"))
+miles=km*0.621371
+print("kilometres:",km)
+print("Miles:",miles)
