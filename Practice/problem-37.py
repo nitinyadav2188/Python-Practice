c70a47a9170aa6db8a9a22e2f179796d8d735d2a
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-08-31 22:53:50 +0530

    Add files via upload

---
+++ Practice/problem-37.py
@@ -0,0 +1,19 @@
+#   *
+#   **
+#   ***
+#   ****
+
+for i in range(1,8):
+    for j in range(1,i):
+        print("*",end='')
+    print()
+
+print("----------------------------------------------------------")
+
+#  ***
+#  ***
+#  ***
+for a in range(3):
+    for b in range(5,8):
+        print("*",end='')
+    print()
