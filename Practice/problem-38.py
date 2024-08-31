aafe215715df5c51efa51dc63e1122650bbd1cf7
Nitin Yadav <151751853+nitinyadav2188@users.noreply.github.com>
2024-08-31 22:54:15 +0530

    Add files via upload

---
+++ Practice/problem-38.py
@@ -0,0 +1,9 @@
+#program that searches for prime numbers from 15 through 25:
+for num in range(15,25):
+    for i in range(2,num):
+        if num%i==0:
+            j=num/i
+            print("Found a factor(",i,")for",num)
+            break
+    else:
+        print(num,"is a prime number") 
